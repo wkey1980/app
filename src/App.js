c271c208a02eea74efc814023c64442a9cfd469d
Will <28814921+wkey1980@users.noreply.github.com>
2021-07-19 17:08:26 +0100

    Building the React App navigation & pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+// Import Pages
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+
+
+
 function App() {
   return (
-    <div className="App">
 
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/blog">
+            <Blog />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/portfolio">
+            <Portfolio />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
