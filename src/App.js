5b7486a1eb66c17555f6af68679540c04008bd5f
Will <28814921+wkey1980@users.noreply.github.com>
2021-07-19 12:28:33 +0100

    Reset project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-// Import Global Styles
-import GlobalStyle from './GlobalStyle';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
-
 function App() {
   return (
     <div className="App">
-      <GlobalStyle />
-      <h1>Setup Project - All Pages Header</h1>
-
-
 
     </div>
   );
