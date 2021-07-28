e1754d3c60b7a8074156d1f46f266ce9ca003824
Will <28814921+wkey1980@users.noreply.github.com>
2021-07-28 14:02:28 +0100

    Learning Material-ui Icons, buttons, container, typography

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Typography, Button, Container } from '@material-ui/core'
+
+// Import Icons
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+
 
 export default function Create() {
   return (
-    <div>
-      Create page
-    </div>
+    <Container>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      <Button
+        onClick={() => console.log('You Have Been Clicked')}
+        type="submit"
+        color="secondary"
+        variant="contained"
+        endIcon={<KeyboardArrowRightIcon />}
+      >
+        Submit
+      </Button>
+
+    </Container>
   )
 }
